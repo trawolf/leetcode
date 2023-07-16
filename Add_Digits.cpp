609fbbb74718c5dc2b1a3fe29c3fb686b55dc180
hanqing <huang010706@126.com>
2023-07-16 20:17:35 +0800

    wrote a Add_Digits solution

---
+++ Add_Digits.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int addDigits(int num) {
+    // std::cout<<num<<':';
+    int add=0;
+    while(num){
+        add+=num%10;
+        num/=10;
+    }
+    // std::cout<<add<<" and "<<num<<std::endl;
+    if(add<10){
+        return add;
+    }
+    else{
+        return addDigits(add);
+    }
+}
+
+int main(){
+    // std::cout<<38%10<<' '<<38/10<<std::endl;
+    std::cout<<addDigits(38)<<std::endl;
+}
