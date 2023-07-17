c4118d025903ba8269ab3ce542b585265898b889
hanqing <huang010706@126.com>
2023-07-17 13:19:29 +0800

    git commit -m "Middle_of_The_Linked_List solution"

---
+++ Middle_of_The_Linked_List.cpp
@@ -0,0 +1,17 @@
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+#include<iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* middleNode(ListNode* head) {
+            
+}
